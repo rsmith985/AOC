81b760fcc039f5c110295e45eea4e5af0047e4fb
Ryan R Smith <rsmith985@gmail.com>
2023-12-01 09:33:52 -0500

    Made abstract interface so future days are easier

--- 2023/Runner.cs
+++ 2023/Runner.cs
@@ -1,5 +1,29 @@
-﻿using rsmith985.AOC._2023;
+﻿using System.Reflection;
+using rsmith985.AOC._2023;
 
 Console.WriteLine("Running");
-Day1.Run();
-Console.ReadLine();
+
+var type = typeof(Day);
+var assembly = Assembly.GetExecutingAssembly();
+var days = new List<Day>();
+foreach(var t in assembly.GetTypes())
+{
+    if(t.IsSubclassOf(type) && !t.IsAbstract)
+    {
+        var day = (Day)Activator.CreateInstance(t);
+        days.Add(day);
+    }
+}
+
+days = days.OrderBy(i => i.Num).ToList();
+
+var last = days.Last();
+last.Run();
+
+Console.WriteLine("Run All? Y/N");
+var input = Console.ReadLine();
+if(input.ToLower() == "y")
+{
+    foreach(var day in days)
+        day.Run();
+}
