a134b16daf67332e245ed8f441422b55458696e3
Ryan R Smith <rsmith985@gmail.com>
2023-12-01 09:35:15 -0500

    forgot to add files

---
+++ 2023/Day.cs
@@ -0,0 +1,55 @@
+﻿namespace rsmith985.AOC._2023;
+
+public abstract class Day
+{
+    public int Num { get; }
+
+    private string _file => $"../../../inputs/input{this.Num}.txt";
+
+    public Day()
+    {
+        var type = this.GetType();
+        var name = type.Name;
+        this.Num = int.Parse(name[3..]);
+    }
+
+    public abstract object Part1();
+    public abstract object Part2();
+
+    public void Run()
+    {
+        if(!File.Exists(_file))
+        {
+            Console.WriteLine($"Couldn't find inputs/input{this.Num}.txt");
+            Environment.Exit(1);
+        }
+
+        var part1 = this.Part1();
+        var part2 = this.Part2();
+        Console.WriteLine($"Day {this.Num.ToString().PadLeft(2, '0')} | Part 1: {part1}");
+        Console.WriteLine($"Day {this.Num.ToString().PadLeft(2, '0')} | Part 2: {part2}");
+        Console.WriteLine();
+    }
+
+    protected StreamReader GetReader() => new StreamReader(_file);
+    protected string[] GetLines() => File.ReadAllLines(_file);
+
+    protected List<string[]> GetWordsPerLine()
+    {
+        var rv = new List<string[]>();
+        foreach(var line in GetLines())
+        {
+            var words = line.Split(' ');
+            rv.Add(words);
+        }
+        return rv;
+    }
+
+    protected List<int> GetIntList()
+    {
+        var rv = new List<int>();
+        foreach(var line in GetLines())
+            rv.Add(int.Parse(line));
+        return rv;
+    }
+}
---
+++ 2023/Days/Day01.cs
@@ -0,0 +1,72 @@
+﻿
+namespace rsmith985.AOC._2023;
+
+public class Day01 : Day
+{
+    public override object Part1()
+    {
+        var tot = 0;
+        foreach(var line in this.GetLines())
+        {
+            var first = -1;
+            var last = -1;
+            foreach (var c in line)
+            {
+                if (int.TryParse(c.ToString(), out int val))
+                {
+                    if (first < 0)
+                        first = last = val;
+                    else
+                        last = val;
+                }
+            }
+
+            if (first > 0)
+                tot += (first * 10 + last);
+        }
+
+        return tot;
+    }
+
+    public override object Part2()
+    {
+        var lookup = new Dictionary<string, int>(){
+            { "one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5},
+            {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}
+        };
+
+        var tot = 0;        
+        foreach(var line in this.GetLines())
+        {
+            var first = -1;
+            var last = -1;
+            var txt = "";
+            foreach (var c in line)
+            {
+                int next = -1;
+                if (int.TryParse(c.ToString(), out int val))
+                    next = val;
+                else
+                {
+                    txt += c.ToString();
+                    foreach (var key in lookup.Keys)
+                        if (txt.EndsWith(key))
+                            next = lookup[key];
+                }
+
+                if (next > 0)
+                {
+                    if (first < 0)
+                        first = last = next;
+                    else
+                        last = next;
+                }
+            }
+
+            if (first > 0)
+                tot += (first * 10 + last);
+        }
+
+        return tot;
+    }
+}
