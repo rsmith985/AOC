ce97c87ad949e12fc5bf0c9b32d1314547fc64e6
Ryan R Smith <rsmith985@gmail.com>
2023-12-01 08:02:52 -0500

    reorganization

---
+++ 2023/Runner.cs
@@ -0,0 +1,5 @@
+﻿using rsmith985.AOC._2023;
+
+Console.WriteLine("Running");
+Day1.Run();
+Console.ReadLine();
--- AOC_2023/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using AOC_2023;
-
-Console.WriteLine("Running");
-Day1.Part1();
-Day1.Part2();
-Console.ReadLine();
